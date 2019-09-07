b5c2a86693b211d7b922e863443e7fa3f84db59b
Claudine <claudinos1990@gmail.com>
2019-09-07 12:29:13 +0200

    add main app

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,22 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args){
+        System.out.println("Welcome to this App");
+        System.out.println("Enter the message");
+        Scanner cipher=new Scanner(System.in);
+        String message=cipher.nextLine();
+        System.out.println("Enter a key");
+        int key=cipher.nextInt();
+        String encryptedWord= encrypt.encryption(message,key);
+        String decryptedWord= decrypt.decryption(encryptedWord,key);
+        System.out.println(encryptedWord);
+        System.out.println(decryptedWord);
 
 
+
+    }
+
+
+}
 
