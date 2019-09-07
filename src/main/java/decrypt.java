0b213903377d27ea0059c10d27062bb909b3a5ab
Claudine <claudinos1990@gmail.com>
2019-09-07 12:28:47 +0200

    add decrypt

---
+++ src/main/java/decrypt.java
@@ -0,0 +1,33 @@
+public class decrypt {
+    public  static String decryption( String plainText,int key){
+        String cipherText="";
+        //
+        for (int i=0;i<plainText.toCharArray().length;i++){
+            char alph=plainText.charAt(i);
+            if (Character.isLetter(alph)){
+                if (Character.isLowerCase(alph)){
+                    char txt=(char)(alph-key);
+                    if (txt<'a'){
+                        cipherText=cipherText+(char)(alph+(26-key));
+                    }else {
+                        cipherText=cipherText+txt;
+                    }
+                }
+                else if (Character.isUpperCase(alph)){
+                    char txt=(char)(alph-key);
+                    if (txt<'A'){
+                        cipherText=cipherText+(char)(alph+(26-key));
+                    }else {
+                        cipherText=cipherText+txt;
+                    }
+                }
+            }
+            else {
+                cipherText=cipherText+alph;
+            }
+        }
+        return cipherText;
+    }
+
+
+}
