41d1b21ce7f771b014e38381b620badcd7b8a6ee
Claudine <claudinos1990@gmail.com>
2019-09-10 10:52:14 +0200

    add encrypted test

--- src/test/java/encryptTest.java
+++ src/test/java/encryptTest.java
@@ -1,1 +1,15 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
+public class encryptTest {
+
+    @Test
+    public void testEncrypt() {
+        encrypt testEncrypt = new encrypt();
+        String Output = "hi";
+        assertEquals(Output, testEncrypt.encry( "hello",  4 ));
+    }
+
+
+
+}
