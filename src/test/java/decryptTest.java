304754db261cea1e913fcc76ceefb3452e4954ec
Claudine <claudinos1990@gmail.com>
2019-09-10 10:48:06 +0200

    add decryped test

--- src/test/java/decryptTest.java
+++ src/test/java/decryptTest.java
@@ -1,2 +1,14 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
 public class decryptTest {
+
+    @Test
+    public void testDecrypt() {
+        decrypt testDecrypt = new decrypt();
+        String Output = "hi";
+        assertEquals(Output, testDecrypt.decry( "hello",  4 ));
+    }
+
+
 }
